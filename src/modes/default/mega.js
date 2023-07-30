40c3bd7646c0539fd5e90045f328dc684f76b33d
Phoenix132 <83040503+Phoenix132@users.noreply.github.com>
2023-07-29 22:25:46 -0400

    Update mega.js

--- src/modes/default/mega.js
+++ src/modes/default/mega.js
@@ -1,7 +1,7 @@
 /** @type {typeof import("../../physics/engine").DefaultSettings} */
 module.exports = {
     TIME_SCALE: 1.2, // magic that make everything work like a certain ball game
-    PLAYER_MAX_CELLS: 64,
+    PLAYER_MAX_CELLS: 512,
     PLAYER_MERGE_NEW_VER: true,
     PLAYER_AUTOSPLIT_SIZE: 0,
     PLAYER_MERGE_TIME: 5,
@@ -11,13 +11,13 @@ module.exports = {
     VIRUS_MONOTONE_POP: true,
     EJECT_SIZE: 38,
     EJECT_LOSS: 38.2,
-    EJECT_DELAY: 50,
+    EJECT_DELAY: 1,
     BOTS: 50,
     BOT_SPAWN_SIZE: 1000,
     EJECT_DISPERSION: 0,
     PELLET_COUNT: 10000,
     PLAYER_VIEW_SCALE: 1.2,
-    PLAYER_SPAWN_SIZE: 500,
+    PLAYER_SPAWN_SIZE: 5020,
     PLAYER_SPAWN_DELAY: 1500,
     PLAYER_SPLIT_DIST: 70,
     DECAY_MIN: 500,
